bf2719fcb3923dcbe4210b585406d9bbbb7b3876
Vinh Ngu <vinh-ngu@hotmail.com>
2021-12-28 19:57:21 +0100

    Add basic logic to test hearing.

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+import os
+import random
 from enum import Enum
 from os import walk
 
-file_path = 'audiofiles'
-filenames = next(walk(file_path), (None, None, []))[2]
-filenames = list(filter(lambda c: c.endswith('mp3'), filenames))
-
-filenames.sort()
+from pydub import AudioSegment
+from pydub.playback import play
 
 
 class Difficulty(Enum):
@@ -14,5 +13,39 @@ class Difficulty(Enum):
     HARD = 5
 
 
-for index in range(len(filenames)):
-    print(index + 1, filenames[index])
+class Pitcher():
+
+    def __init__(self):
+        audiofile_path = 'audiofiles'
+        filenames = next(walk(audiofile_path), (None, None, []))[2]
+        filenames = list(filter(lambda c: c.endswith('mp3'), filenames))
+        filenames.sort()
+
+        self.audiofiles = [os.path.join(audiofile_path, filename) for filename in filenames]
+
+    def get_index_of_tone(self, key):
+        for f in self.audiofiles:
+            tone = f.split('_')[1].split('.')[0]
+            if tone == key:
+                return self.audiofiles.index(f)
+
+    def get_similar_tones(self, tone: str, difficulty: Difficulty):
+        index = self.get_index_of_tone(tone)
+        if not index:
+            print("Wrong tone provided. Try again!")
+            return
+
+        random_indexes = []
+        for i in range(5):
+            random_indexes.append(random.randint(index - difficulty.value, index + difficulty.value))
+
+        return [self.audiofiles[random_index] for random_index in random_indexes]
+
+
+if __name__ == '__main__':
+    pitcher = Pitcher()
+    tones = pitcher.get_similar_tones('c5', Difficulty.EASY)
+
+    for t in tones:
+        song = AudioSegment.from_mp3(t)
+        play(song)
